429530ea7690e49913a27e5413cebaf3c76f5bf4
harini-y-ops <harini.n20061@gmail.com>
2026-03-01 15:23:59 +0530

    square_cube.py

---
+++ level1-basics/square_cube.py
@@ -0,0 +1,6 @@
+#Choose a number and print its square and cube root 
+num = int(input("Enter a number: "))
+square = num ** 2
+cube = num ** 3
+print(f"Square of {num} is: {square}")
+print(f"Cube of {num} is: {cube}")

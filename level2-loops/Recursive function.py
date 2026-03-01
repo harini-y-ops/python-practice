244ec069c366f0a3b41ce11cebf434c3d013310c
harini-y-ops <harini.n20061@gmail.com>
2026-03-01 17:54:15 +0530

    Create Recursive function.py

---
+++ level2-loops/Recursive function.py
@@ -0,0 +1,7 @@
+#Write a recursive function to calculate factorial
+def factorial(n):
+    if n==1:
+        return 1
+    return n*factorial(n-1)
+n=int(input("Enter the number: "))
+print(factorial(n))

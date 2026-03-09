fb464514446411c3a45183b9131b87e37c594c9f
harini-y-ops <harini.n20061@gmail.com>
2026-03-09 22:24:53 +0530

    Create first_letter.py

---
+++ List_comprehension/first_letter.py
@@ -0,0 +1,6 @@
+#sentence = "artificial intelligence is powerful"
+#Using list comprehension, create a list containing the first letter of each word.
+sentence = "artificial intelligence is powerful"
+word=sentence.split()
+letters=[word[0] for word in words]
+print(letters)
